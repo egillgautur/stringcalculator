14fe1b18b2ab53a6c83a3d5279d30d4db1f99e3d
Egill Gautur <egills14@ru.is>
2015-10-17 17:05:28 +0000

    test 1

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -19,14 +19,16 @@ public class CalculatorTest {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	/*
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
-    }
+    }*/
 
 }
